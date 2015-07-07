2398544aefa573f44e61f72dd6017e899614414c
Marcus Day <110797@student.sae.edu.au>
2015-07-07 16:05:35 +1000

    Added magic area
    Added shoot in magic area
    
    Changes some names and added folders

--- Major Project Prototype/Assets/Scripts/Player/PlayerMove.cs
+++ Major Project Prototype/Assets/Scripts/Player/PlayerMove.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Move : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
 
     public float speed;
--- Major Project Prototype/Assets/Scripts/Player/PlayerShoot.cs
+++ Major Project Prototype/Assets/Scripts/Player/PlayerShoot.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
 
+    bool canShoot;
+
+    public float shootSpeed;
+
+    public Transform shotSpot;
+
+    public GameObject shotBullet;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 	
 	}
+
+    void FixedUpdate()
+    {
+        if (canShoot == true)
+        {
+            if(Input.GetMouseButton(0))
+            {
+               Instantiate(shotBullet, shotSpot.position, Quaternion.identity);
+            }
+        }
+    }
+        
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
+
 	
 	}
+
+    void OnTriggerStay(Collider col) 
+    {
+        if (col.gameObject.tag == "Magic Area")
+        {
+            print("Im in magic");
+
+            canShoot = true;
+        }
+        else
+        {
+            canShoot = false;
+        }
+
+    }
 }
