b122802611504a1447adb2e9f54e9d0e5f212c15
David Hanna <onnfire@hotmail.com>
2015-07-16 16:04:54 +1000

    object script for box prototype

---
+++ Major Project Prototype/Assets/Scripts/Interactables/Object.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Object : MonoBehaviour {
+
+	public bool bIsObjectLight = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
